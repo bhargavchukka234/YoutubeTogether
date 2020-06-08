eca8817e725710294e6a4bf02f8b67def967d10d
apoorva-uci <amuthine@uci.edu>
2020-06-07 17:00:37 -0700

    Added crossorigin annotation

--- src/main/java/com/distri/mdlware/service/VirtualRoomService.java
+++ src/main/java/com/distri/mdlware/service/VirtualRoomService.java
@@ -1,33 +1,37 @@
 package com.distri.mdlware.service;
 
-import com.distri.mdlware.dto.NewClientDTO;
+import java.util.UUID;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.distri.mdlware.dto.NewClientDTO;
+
 import uci.middleware.project.dao.ClientDAO;
 import uci.middleware.project.dao.RoomDAO;
 import uci.middleware.project.dto.Room;
 
-import java.util.UUID;
-
 @RestController
 public class VirtualRoomService {
 
-    @Autowired
-    private RoomDAO roomDAO;
+	@Autowired
+	private RoomDAO roomDAO;
 
-    @Autowired
-    private ClientDAO clientDAO;
+	@Autowired
+	private ClientDAO clientDAO;
 
-    @PostMapping("/create/room/{roomName}")
-    public ResponseEntity<NewClientDTO> createRoom(@PathVariable String roomName){
+	@CrossOrigin
+	@PostMapping("/create/room/{roomName}")
+	public ResponseEntity<NewClientDTO> createRoom(@PathVariable String roomName) {
 
-        Room room = roomDAO.createRoom(roomName, new Room());
-        String clientId = UUID.randomUUID().toString();
-        roomDAO.addClientToRoom(roomName , clientId);
-        return new ResponseEntity<>(new NewClientDTO(clientId, new Room()), HttpStatus.OK);
-    }
+		Room room = roomDAO.createRoom(roomName, new Room());
+		String clientId = UUID.randomUUID().toString();
+		roomDAO.addClientToRoom(roomName, clientId);
+		return new ResponseEntity<>(new NewClientDTO(clientId, new Room()), HttpStatus.OK);
+	}
 }
